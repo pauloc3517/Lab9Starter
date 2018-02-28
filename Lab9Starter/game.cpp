ea7f7efb3bb2f2829efd3766e731ccc84a7f59a7
Peter Lowe <peter.lowe@itcarlow.ie>
2018-02-28 18:29:45 +0000

    Initial commit

--- Lab9Starter/game.cpp
+++ Lab9Starter/game.cpp
@@ -5,115 +5,82 @@
 
 
 
+#include "Game.h"
+#include <iostream>
+
+
+
 Game::Game() :
-	m_window{ sf::VideoMode{ 800, 600, 32 }, "SFML Game" },
-	m_exitGame{false} //when true game will exit
-{
-	setupFontAndText(); // load font 
-	setupSprite(); // load texture
+	m_window{ sf::VideoMode{ 800, 480 }, "SMFL Game" },
+	m_triangle{ sf::Triangles },
+	m_vertexs{
+		{ { 100,150 },CORNFLOWER_BLUE },
+		{ { 100,350 },CORNFLOWER_BLUE },
+		{ { 200,350 },CORNFLOWER_BLUE }
 }
 
-
-Game::~Game()
 {
+	for (size_t i = 0; i < 3; i++)
+	{
+		m_triangle.append(m_vertexs[i]);
+	}
 }
-
 
 void Game::run()
 {
 	sf::Clock clock;
 	sf::Time timeSinceLastUpdate = sf::Time::Zero;
-	sf::Time timePerFrame = sf::seconds(1.f / 60.f); // 60 fps
+	sf::Time timePerFrame = sf::seconds(1.f / 60.f);
 	while (m_window.isOpen())
 	{
-		processEvents(); // as many as possible
+		processEvents();
 		timeSinceLastUpdate += clock.restart();
 		while (timeSinceLastUpdate > timePerFrame)
 		{
 			timeSinceLastUpdate -= timePerFrame;
-			processEvents(); // at least 60 fps
-			update(timePerFrame); //60 fps
+			processEvents();
+			update(timePerFrame);
 		}
-		render(); // as many as possible
+		render();
 	}
 }
-/// <summary>
-/// handle user and system events/ input
-/// get key presses/ mouse moves etc. from OS
-/// and user :: Don't do game update here
-/// </summary>
+
 void Game::processEvents()
 {
 	sf::Event event;
 	while (m_window.pollEvent(event))
 	{
-		if ( sf::Event::Closed == event.type) // window message
+		if (event.type == sf::Event::Closed)
 		{
 			m_window.close();
 		}
-		if (sf::Event::KeyPressed == event.type) //user key press
-		{
-			if (sf::Keyboard::Escape == event.key.code)
-			{
-				m_exitGame = true;
-			}
-		}
 	}
 }
 
-/// <summary>
-/// Update the game world
-/// </summary>
-/// <param name="t_deltaTime">time interval per frame</param>
-void Game::update(sf::Time t_deltaTime)
+void Game::update(sf::Time)
 {
-	if (m_exitGame)
+	// wrong way to do transformations just here to show you
+	// how the vertexes could be updated you need to maintain
+	// a set of MyVector3Ds and then update the vertex array
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
 	{
-		m_window.close();
+		m_triangle[0].position = m_triangle[0].position + sf::Vector2f(0.0f, 0.7f);
+
+		m_vertexs[1].position = m_vertexs[1].position + sf::Vector2f(1.0f, 1.0f); // this has no effect 
+																				  // because the vertex array made a copy of the vertex when we appended it
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+	{
+		m_triangle[0].position = m_triangle[0].position - sf::Vector2f(0.0f, 0.7f);
+
+		m_vertexs[1].position = m_vertexs[1].position - sf::Vector2f(1.0f, 1.0f);// this has no effect 
+																				 // because the vertex array made a copy of the vertex when we appended it
 	}
 }
 
-/// <summary>
-/// draw the frame and then switch bufers
-/// </summary>
 void Game::render()
 {
-	m_window.clear(sf::Color::White);
-	m_window.draw(m_welcomeMessage);
-	m_window.draw(m_logoSprite);
+	m_window.clear();
+	m_window.draw(m_triangle);
 	m_window.display();
-}
-
-/// <summary>
-/// load the font and setup the text message for screen
-/// </summary>
-void Game::setupFontAndText()
-{
-	if (!m_ArialBlackfont.loadFromFile("ASSETS\\FONTS\\ariblk.ttf"))
-	{
-		std::cout << "problem loading arial black font" << std::endl;
-	}
-	m_welcomeMessage.setFont(m_ArialBlackfont);
-	m_welcomeMessage.setString("SFML Game");
-	m_welcomeMessage.setStyle(sf::Text::Underlined | sf::Text::Italic | sf::Text::Bold);
-	m_welcomeMessage.setPosition(40.0f, 40.0f);
-	m_welcomeMessage.setCharacterSize(80);
-	m_welcomeMessage.setOutlineColor(sf::Color::Red);
-	m_welcomeMessage.setFillColor(sf::Color::Black);
-	m_welcomeMessage.setOutlineThickness(3.0f);
-
-}
-
-/// <summary>
-/// load the texture and setup the sprite for the logo
-/// </summary>
-void Game::setupSprite()
-{
-	if (!m_logoTexture.loadFromFile("ASSETS\\IMAGES\\SFML-LOGO.png"))
-	{
-		// simple error message if previous call fails
-		std::cout << "problem loading logo" << std::endl;
-	}
-	m_logoSprite.setTexture(m_logoTexture);
-	m_logoSprite.setPosition(300.0f, 180.0f);
 }
--- Lab9Starter/game.h
+++ Lab9Starter/game.h
@@ -1,36 +1,28 @@
-// author Peter Lowe
 #ifndef GAME
 #define GAME
+// must write comments for Pete's sake
+
 
 #include <SFML/Graphics.hpp>
+
+
+const sf::Color CORNFLOWER_BLUE{ 154, 206, 235, 255 };
 
 class Game
 {
 public:
 	Game();
-	~Game();
-	/// <summary>
-	/// main method for game
-	/// </summary>
 	void run();
+private:
+	void             processEvents();
+	void             update(sf::Time);
+	void             render();
 
 private:
-
-	void processEvents();
-	void update(sf::Time t_deltaTime);
-	void render();
-	
-	void setupFontAndText();
-	void setupSprite();
-
-	sf::RenderWindow m_window; // main SFML window
-	sf::Font m_ArialBlackfont; // font used by message
-	sf::Text m_welcomeMessage; // text used for message on screen
-	sf::Texture m_logoTexture; // texture used for sfml logo
-	sf::Sprite m_logoSprite; // sprite used for sfml logo
-	bool m_exitGame; // control exiting game
+	sf::RenderWindow m_window;
+	sf::VertexArray m_triangle;
+	sf::Vertex m_vertexs[3]; // only used to initialise the array 
 
 };
 
 #endif // !GAME
-
